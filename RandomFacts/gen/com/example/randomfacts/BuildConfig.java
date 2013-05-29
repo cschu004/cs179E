53e00b0c009a403b5d5c09654663fc7c2e57ba5a
Kevin <kng006@ucr.edu>
2013-05-29 13:34:16 -0700

    Random facts program

---
+++ RandomFacts/gen/com/example/randomfacts/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.randomfacts;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ RandomFacts/src/com/example/randomfacts/Page2.java
@@ -0,0 +1,47 @@
+package com.example.randomfacts;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class Page2 extends Activity {
+	Button button;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.page2);
+		addListenerOnButton();
+	}
+	public void addListenerOnButton() {
+		 
+		final Context context = this;
+ 
+		button = (Button) findViewById(R.id.button1);
+ 
+		button.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View arg0) {
+ 
+			    Intent intent = new Intent(context, Page3.class);
+                            startActivity(intent);   
+ 
+			}
+ 
+		});
+ 
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
